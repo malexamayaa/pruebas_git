a80fc63a8dce30d99b75ab7b5fa9078c9e9308dc
malexamayaa <alejandro_amayaa@hotmail.com>
2025-03-06 13:52:13 +0100

    añadidos cambios en comportamiento del botón

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-alert("Hola, soy una ventanita de Javascript!!!");
+//FUNCIÓN ANÓNIMA QUE SE EJECUTA CUANDO LA VENTANA ACABA DE CARGAR
+window.onload = function(){
+    alert("Hola, soy una ventanita de Javascript!!!");
+
+    let botonito = document.getElementById("btnPrueba");
+    
+    botonito.addEventListener("click", function(){
+        alert("Antes de nada, la clase del botoncito es: " + botonito.className);
+        let nuevaclase = (botonito.className="clase1") ? "clase2" : "clase1";
+        alert("La nueva clase que se va a usar es: " + nuevaclase);
+        botonito.className=nuevaclase;
+        nuevaclase=null;
+        alert("Ahora la clase del botoncito es: " + botonito.className);
+    });
+}
